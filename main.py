76613169517e0f0393af0d534efeaf07b735d9f6
Roger Navarro <roanacla@me.com>
2019-05-15 20:57:15 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ class MineSweeperSquare:
         self.hasAMine = False
         self.refNumber = 0
         self.isVisible = False
-        self.pos = (-1,-1)
-
-    def setPos(self,row,col):
-        self.pos = (row,col)
 
 class MineSweeperEnv(XYEnvironment):
 
@@ -23,59 +19,11 @@ class MineSweeperEnv(XYEnvironment):
         for row in range(self.numOfRows):
             rows = []
             for col in range(self.numOfColumns):
-                mineSquare = MineSweeperSquare()
-                mineSquare.setPos(row,col)
-                rows.append(mineSquare)
+                rows.append(MineSweeperSquare())
             self.map.append(rows)
 
     def startPlaying(self, startRow, startCol):
         self.__addMinesRandomlyStartingAt(startRow, startCol)
-        emptyNeighbors = self.__getNextEmptyNeighbors(startRow, startCol)
-
-    def __getNextEmptyNeighbors(self, rowPos, colPos):
-        row = rowPos - 1
-        col = colPos - 1
-        nextEmptyNeighbors = []
-        if self.map[row][col].hasAMine != True:
-            nextEmptyNeighbors.append(self.map[row][col])
-        nextEmptyNeighbors += self.__exploreSidesStartingAt(row,col)
-        #Repeat process in next row going down.
-
-        while row < self.numOfRows:
-            self.__getNextEmptyNeighbors(rowPos + 1, colPos + 1)
-            row += 1
-
-        while row >= 0:
-
-
-
-        for square in nextEmptyNeighbors:
-            print(square.pos)
-
-    def __exploreSidesStartingAt(self, row, col):
-        # RIGHT
-        emptyNeighbors = []
-        colRight = col + 1
-        while colRight < self.numOfColumns:
-            square = self.map[row][colRight]
-            if square.hasAMine == True:
-                break
-            else:
-                emptyNeighbors.append(square)
-                colRight += 1
-        print("acabe")
-
-        # LEFT
-        colLeft = col - 1
-        while colLeft >= 0:
-            square = self.map[row][colLeft]
-            if square.hasAMine == True:
-                break
-            else:
-                emptyNeighbors.append(square)
-                colLeft -= 1
-
-        return emptyNeighbors
 
     def __increaseByOneTheNeighborsOf(self, colPos, rowPos):  # Once a bomb is added in the environment, the niegbors update their ref number.
         neighbors = self.getObjectsOfNeighborsAt(colPos, rowPos)
@@ -136,7 +84,5 @@ class MineSweeperEnv(XYEnvironment):
                 print("|")
 
 env = MineSweeperEnv(9,9,10)
-env.startPlaying(1,4)
+env.startPlaying(3,6)
 env.printEnvironment()
-
-
